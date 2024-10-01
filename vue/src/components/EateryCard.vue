6516519ce7cca40a1b5d25a919be6bed9f18d41c
Johanna Romain <jromain0708@gmail.com>
2024-10-01 13:04:14 -0400

    Updates

--- vue/src/components/EateryCard.vue
+++ vue/src/components/EateryCard.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h1>{{restaurant.name}}</h1>
-    <p>{{ restaurant.status }}</p>
-    <p>{{ restaurant.cuisine }}</p>
     <img :src="this.img" alt="restaurant image" />
+    <p>{{ restaurant.status ? "Open Now" : "Closed" }}</p>
+    <p>{{ restaurant.cuisine }}</p>
+    <p>{{ restaurant.openTime  }}</p>
+    <p>{{ restaurant.closeTime }}</p>
+    <p>{{ restaurant.address }}</p>
+    <button>Call to Order</button>
+    <button>Add to Group</button>
+    
   </div>
 </template>
 
@@ -12,10 +18,13 @@ export default {
     data() {
         return {
             restaurant: {
-                cuisine: "",
-                name: "",
-                status: "",
-                img: "../../dinerBackground.jpg",
+                cuisine: "Mediterranean",
+                name: "Simply Greek",
+                status: true,
+                img: "../dinerBackground.jpg",
+                openTime: "7:00 AM",
+                closeTime: "9:00 PM",
+                address: "1234 Main St, Anytown, USA"
             }
         }
     }
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
-    <eatery-finder-form-vue/>
-    <eatery-card />
+    <eatery-finder-form-vue />
+    <div class="eatery-grid">
+      <eatery-card v-for="n in 25" v-bind:key="n"/>
+    </div>
   </div>
 </template>
 
 <script>
-import EateryFinderFormVue from '../components/EateryFinderForm.vue';
-import EateryCard from '../components/EateryCard.vue';
+import EateryFinderFormVue from "../components/EateryFinderForm.vue";
+import EateryCard from "../components/EateryCard.vue";
 export default {
   components: {
     EateryFinderFormVue,
-    EateryCard
-  }
+    EateryCard,
+  },
 };
 </script>
+<style scoped>
+.eatery-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  background-color:aqua;
+} 
+.home {
+  width: 100%;
+  
+}
+</style>
