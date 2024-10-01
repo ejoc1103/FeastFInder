a67abe2b9d99bc4f6ea08fd6d28d395493627bf1
Joseph <joehubs@icloud.com>
2024-10-01 10:55:24 -0400

    added eatery view

---
+++ vue/src/components/EateryCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{restaurant.name}}</h1>
+    <p>{{ restaurant.status }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            restaurant: {
+                cuisine: "",
+                name: "",
+                status: "",
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/views/EateriesView.vue
@@ -0,0 +1,16 @@
+<template>
+  <eatery-card/>
+</template>
+
+<script>
+import EateryCard from '../components/EateryCard.vue';
+export default {
+    components: {
+        EateryCard
+    }
+}
+</script>
+
+<style>
+
+</style>
