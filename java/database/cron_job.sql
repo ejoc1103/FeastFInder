34fb520768770007ce6cdf6069799b82cc4a5fff
Joseph <joehubs@icloud.com>
2024-10-08 12:30:32 -0400

    joe is genius

--- java/database/cron_job.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pg_cron;
-
-SELECT cron.schedule('0 0 * * *', $$UPDATE your_table
-SET status = CASE 
-    WHEN due_date <= CURRENT_DATE THEN TRUE
-    ELSE FALSE
-END;$$);
